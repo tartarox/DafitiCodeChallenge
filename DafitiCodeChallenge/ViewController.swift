0fe634e3212adfaa59c382355eb73e89d98e0b23
Fabio Takahara <yudi@nutrisoft.com.br>
2016-09-21 10:43:19 -0300

    Initial Commit

---
+++ DafitiCodeChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DafitiCodeChallenge
+//
+//  Created by MacBook on 21/09/16.
+//  Copyright © 2016 Fabio Yudi Takahara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
